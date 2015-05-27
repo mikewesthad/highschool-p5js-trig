5b22afb84c110b1d2ceb60ab94f456854ecee7a0
Michael Hadley <michaelwesthadley@gmail.com>
2015-05-27 15:24:39 -0500

    oscilloscope sketch added

--- micSpectrum.js
+++ micSpectrum.js
@@ -1,26 +1,55 @@
-var input;
-var analyzer;
+var mic, fft;
 
 function setup() {
-  createCanvas(710, 200);
+  createCanvas(window.innerWidth, window.innerHeight);
 
-  // Create an Audio input
+  // Create an FFT object
+  fft = new p5.FFT();
+
+  // Get the mic and hook it into the FFT object
   mic = new p5.AudioIn();
-
-  // start the Audio Input.
-  // By default, it does not .connect() (to the computer speakers)
   mic.start();
+  mic.connect(fft);
+}
+
+// Resize the canvas to match the window size
+function windowResized() {
+  resizeCanvas(window.innerWidth, window.innerHeight);
 }
 
 function draw() {
-  background(200);
+  // Fill the background with a dark color
+  background(20);
 
-  // Get the overall volume (between 0 and 1.0)
-  var vol = mic.getLevel();
-  fill(127);
-  stroke(0);
+  // Draw some gridlines on the background
+  stroke(75);
+  strokeWeight(0.5);
+  var boxWidth = 50;
+  var dim = max(width, height);
+  var lines = ceil(dim/boxWidth);
+  for (var i = 0; i <= lines; i++) {
+    if (i % 4 == 0) {
+      strokeWeight(2);
+    }
+    else {
+      strokeWeight(0.5);
+    }
+    var d = i*boxWidth;
+    line(0, d, width, d);
+    line(d, 0, d, height);
+  }
 
-  // Draw an ellipse with height based on volume
-  var h = map(vol, 0, 1, height, 0);
-  ellipse(width/2, h - 25, 50, 50);
+  // Get the waveform from the FFT
+  var waveform = fft.waveform();
+  noFill();
+  beginShape();
+  stroke(0,255,0); // waveform is red
+  strokeWeight(5);
+  for (var i = 0; i< waveform.length; i++){
+    var x = map(i, 0, waveform.length, 0, width);
+    var y = map( waveform[i], 0, 255, 0, height) + 5;
+    vertex(x,y);
+  }
+  endShape();
+
 }
